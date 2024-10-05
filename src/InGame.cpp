79a691fd3664bba0e0f3bb23eefc58ac55d0461f
quadtree <quadtree@gmail.com>
2024-10-04 18:33:14 -0700

    U

---
+++ src/InGame.cpp
@@ -0,0 +1,68 @@
+#include <LD56.h>
+
+void UpdateWorldState()
+{
+    // cout << "UPDATE world state! " << ticksHandledByGameStateUpdates << endl;
+}
+
+void GameUpdateThread()
+{
+    auto ticksPerGameUpdate = SDL_GetPerformanceFrequency() / 60;
+
+    Uint64 lastSecond = 0;
+    auto updatesLastSecond = 0;
+
+    ticksHandledByGameStateUpdates = SDL_GetPerformanceCounter();
+
+    while (true)
+    {
+        if (SDL_GetPerformanceCounter() > ticksHandledByGameStateUpdates)
+        {
+            ticksHandledByGameStateUpdates += ticksPerGameUpdate;
+            UpdateWorldState();
+
+            updatesLastSecond++;
+        }
+        else
+        {
+            SDL_Delay(1);
+        }
+
+        auto currentSecond = SDL_GetTicks64() / 1000;
+        if (currentSecond != lastSecond)
+        {
+            cout << "Game Update FPS=" << updatesLastSecond << endl;
+            updatesLastSecond = 0;
+            lastSecond = currentSecond;
+        }
+    }
+}
+
+void InGameMainLoop()
+{
+    SDL_Event evt;
+    while (SDL_PollEvent(&evt))
+    {
+    }
+
+    SDL_SetRenderDrawColor(rnd, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(rnd);
+
+    SDL_FRect trg;
+    trg.x = 20;
+    trg.y = 20;
+    trg.w = 32;
+    trg.h = 32;
+
+    SDL_RenderCopyExF(rnd, testImage.get(), nullptr, &trg, 20, nullptr, SDL_FLIP_NONE);
+
+    SDL_RenderPresent(rnd);
+}
+
+void EnterInGameState()
+{
+    cout << "About to call emscripten_set_main_loop" << endl;
+	emscripten_set_main_loop(InGameMainLoop, 0, 0);
+
+    gameUpdateThread = thread(GameUpdateThread);
+}
--- src/LD56.cpp
+++ src/LD56.cpp
@@ -27,69 +27,8 @@ thread::id mainThreadId;
 
 Uint64 ticksHandledByGameStateUpdates;
 
-void UpdateWorldState()
-{
-	// cout << "UPDATE world state! " << ticksHandledByGameStateUpdates << endl;
-}
-
-void GameUpdateThread()
-{
-	auto ticksPerGameUpdate = SDL_GetPerformanceFrequency() / 60;
-
-	Uint64 lastSecond = 0;
-	auto updatesLastSecond = 0;
-
-	ticksHandledByGameStateUpdates = SDL_GetPerformanceCounter();
-
-	while (true)
-	{
-		if (SDL_GetPerformanceCounter() > ticksHandledByGameStateUpdates)
-		{
-			ticksHandledByGameStateUpdates += ticksPerGameUpdate;
-			UpdateWorldState();
-
-			updatesLastSecond++;
-		}
-		else
-		{
-			SDL_Delay(1);
-		}
-
-		auto currentSecond = SDL_GetTicks64() / 1000;
-		if (currentSecond != lastSecond)
-		{
-			cout << "Game Update FPS=" << updatesLastSecond << endl;
-			updatesLastSecond = 0;
-			lastSecond = currentSecond;
-		}
-	}
-}
-
-void MainLoop()
-{
-	SDL_Event evt;
-	while (SDL_PollEvent(&evt))
-	{
-	}
-
-	SDL_SetRenderDrawColor(rnd, 0, 0, 0, SDL_ALPHA_OPAQUE);
-	SDL_RenderClear(rnd);
-
-	SDL_FRect trg;
-	trg.x = 20;
-	trg.y = 20;
-	trg.w = 32;
-	trg.h = 32;
-
-	SDL_RenderCopyExF(rnd, testImage.get(), nullptr, &trg, 20, nullptr, SDL_FLIP_NONE);
-
-	SDL_RenderPresent(rnd);
-}
-
-void AltThreadEntryPoint()
-{
-	cout << "some other thread" << endl;
-}
+void InGameMainLoop();
+void EnterInGameState();
 
 int main(int argc, char *argv[])
 {
@@ -103,12 +42,9 @@ int main(int argc, char *argv[])
 
 	wnd = SDL_CreateWindow("LD56", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, SDL_WINDOW_RESIZABLE);
 
-	cout << "About to call emscripten_set_main_loop" << endl;
-	emscripten_set_main_loop(MainLoop, 0, 0);
+	EnterInGameState();
 
 	rnd = SDL_CreateRenderer(wnd, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-
-	gameUpdateThread = thread(GameUpdateThread);
 
 	testImage = LoadTexture("assets/xt3.xcf");
 
