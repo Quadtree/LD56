de9ea56693e7168bafc5c24a1b32579aad8db973
quadtree <quadtree@gmail.com>
2024-10-04 19:03:28 -0700

    U

--- src/GameState/Bacteria.cpp
+++ src/GameState/Bacteria.cpp
@@ -1,5 +1,7 @@
 #include "Bacteria.h"
 
-void Bacteria::Update1(Bacteria &nextState, std::function<void(std::function<void(class GameState &)>)>& queueMutation) const
+void Bacteria::Update1(Bacteria &nextState, std::function<void(std::function<void(class GameState &)>)> &queueMutation) const
 {
+    nextState = *this;
+    nextState.Position = Vector2(Position.X, Position.Y + 1);
 }
