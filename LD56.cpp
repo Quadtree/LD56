81b5da7cbd27992a651b19ee29432f7d99fb5453
Quadtree <quadtree@gmail.com>
2024-10-04 17:03:14 -0700

    U

---
+++ LD56.cpp
@@ -0,0 +1,12 @@
+﻿// LD56.cpp : Defines the entry point for the application.
+//
+
+#include "LD56.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ LD56.h
@@ -0,0 +1,8 @@
+﻿// LD56.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
