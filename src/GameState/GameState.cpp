a2dbb54e92328bcb6a916f05d90c49d7bf62d94b
quadtree <quadtree@gmail.com>
2024-10-04 19:16:20 -0700

    U

--- src/GameState/GameState.cpp
+++ src/GameState/GameState.cpp
@@ -27,7 +27,7 @@ void GameState::DoUpdate(GameState &nextGameState)
         auto startPos = i;
         auto endPos = min(i + PROCESSING_BLOCK_SIZE, NumActiveBacteria);
 
-        SubmitToThreadPool([startPos, endPos, queueMutation]()
+        SubmitToThreadPool([startPos, endPos, queueMutation, nextGameState]()
                            {
             for (int j = startPos; j < endPos; ++j)
             {
