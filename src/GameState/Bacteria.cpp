597e6e95e13a9ce2689a198887960f994f1df048
quadtree <quadtree@gmail.com>
2024-10-05 10:54:27 -0700

    U

--- src/GameState/Bacteria.cpp
+++ src/GameState/Bacteria.cpp
@@ -3,7 +3,7 @@
 void Bacteria::Update1(Bacteria &nextState, class MutationQueue *queueMutation) const
 {
     nextState = *this;
-    nextState.Position = Vector2(Position.X, Position.Y + 1);
+    // nextState.Position = Vector2(Position.X, Position.Y + 1);
 
 #if _DEBUG
     nextState.NumUpdates = NumUpdates + 1;
