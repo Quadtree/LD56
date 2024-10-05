beb00d405c090ff0b9f7114c308fe4e986e3a71c
quadtree <quadtree@gmail.com>
2024-10-04 17:48:28 -0700

    U

--- scripts/server.py
+++ scripts/server.py
@@ -3,7 +3,12 @@ import http.server
 class RequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         super().do_GET()
+
         pass
+
+    def end_headers(self):
+        self.send_header("X-Test-Header", "Y")
+        super().end_headers()
 
 server = http.server.HTTPServer(('0.0.0.0', 8080), RequestHandler)
 server.serve_forever()
