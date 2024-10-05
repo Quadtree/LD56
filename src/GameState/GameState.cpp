f70798efe025fc776af95f0bae921edb4855e015
quadtree <quadtree@gmail.com>
2024-10-04 18:57:59 -0700

    U

--- src/GameState/GameState.cpp
+++ src/GameState/GameState.cpp
@@ -3,6 +3,14 @@
 
 GameState::GameState() : NumActiveBacteria(0)
 {
+    NumActiveBacteria = 100;
+
+    for (int i = 0; i < NumActiveBacteria; ++i)
+    {
+        BacteriaList[i].Position = Vector2(10 * i, 20);
+        BacteriaList[i].Health = 1;
+        BacteriaList[i].Faction = 0;
+    }
 }
 
 void GameState::DoUpdate(GameState &nextGameState)
