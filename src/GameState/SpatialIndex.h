b369eb02daeadfce29f19e7a485eb3a377ebfe79
quadtree <quadtree@gmail.com>
2024-10-05 11:37:46 -0700

    U

--- src/GameState/SpatialIndex.h
+++ src/GameState/SpatialIndex.h
@@ -14,4 +14,20 @@ class SpatialIndex
 {
 public:
     GridCell Cells[(GridSize / Radius) * (GridSize / Radius)];
+
+    int32_t CellXYToCellID(int32_t x, int32_t y)
+    {
+        return y * (GridSize / Radius) + x;
+    }
+
+    void ClearCells()
+    {
+        for (auto y = 0; y < GridSize / Radius; ++y)
+        {
+            for (auto y = 0; y < GridSize / Radius; ++y)
+            {
+                Cells[CellXYToCellID(x, y)]->NumInCell = 0;
+            }
+        }
+    }
 };
