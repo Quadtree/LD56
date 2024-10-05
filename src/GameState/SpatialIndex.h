5275f075c95e35ea8d154de21a96e51b3a1ab10c
quadtree <quadtree@gmail.com>
2024-10-05 11:34:47 -0700

    U

---
+++ src/GameState/SpatialIndex.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define MAX_PER_CELL 20
+
+class GridCell
+{
+public:
+    Bacteria List[MAX_PER_CELL];
+    int NumInCell;
+};
+
+template <int Radius, int GridSize>
+class SpatialIndex
+{
+public:
+    GridCell Cells[(GridSize / Radius) * (GridSize / Radius)]
+};
