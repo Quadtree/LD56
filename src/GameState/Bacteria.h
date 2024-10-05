41c90b2d8d0532159f5f6e89dd9c264840a61891
quadtree <quadtree@gmail.com>
2024-10-04 18:47:45 -0700

    U

--- src/GameState/Bacteria.h
+++ src/GameState/Bacteria.h
@@ -8,4 +8,6 @@ class Bacteria
     Vector2 Velocity;
     uint8_t Faction;
     uint8_t Health;
+
+    void Update1(const Bacteria &currentState, Bacteria &nextState, function<void, function<void, GameState &>> queueMutation);
 };
