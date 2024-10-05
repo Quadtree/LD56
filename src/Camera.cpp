b945d2252c9f7b50a2d674d092d1f3fdd8ddec97
quadtree <quadtree@gmail.com>
2024-10-05 01:31:57 -0700

    U

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -3,3 +3,18 @@
 Camera::Camera() : ZoomLevel(10)
 {
 }
+
+Vector2 Camera::ScreenToReal(Vector2 screen)
+{
+    return Vector2();
+}
+
+Vector2 Camera::RealToScreen(Vector2 screen)
+{
+    return Vector2();
+}
+
+float Camera::RealToScreenScale(float inputValue)
+{
+    return inputValue * ZoomLevel;
+}
