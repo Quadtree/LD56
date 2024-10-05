6f40fd304259b5e96bacf69348be00779aec2a14
quadtree <quadtree@gmail.com>
2024-10-04 18:42:55 -0700

    U

--- src/GameState/GameState.h
+++ src/GameState/GameState.h
@@ -2,11 +2,15 @@
 
 #include <mutex>
 
+#define MAX_BACTERIA 1024
+
 class GameState
 {
 public:
     // this mutex is held either by the renderer, or by the game logic updater as it does an update
     std::mutex mutex;
 
-    void DoUpdate(GameState& nextGameState);
+    void DoUpdate(GameState &nextGameState);
+
+    Bacteria BacteriaList[MAX_BACTERIA];
 };
