5266759df762c5b836af37a33a89dd391b040519
quadtree <quadtree@gmail.com>
2024-10-04 18:41:41 -0700

    U

---
+++ src/GameState/GameState.cpp
@@ -0,0 +1,5 @@
+#include "GameState.h"
+
+void GameState::DoUpdate(GameState &nextGameState)
+{
+}
--- src/GameState/GameState.h
+++ src/GameState/GameState.h
@@ -7,4 +7,6 @@ class GameState
 public:
     // this mutex is held either by the renderer, or by the game logic updater as it does an update
     std::mutex mutex;
+
+    void DoUpdate(GameState& nextGameState);
 };
