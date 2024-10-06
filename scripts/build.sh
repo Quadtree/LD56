0ab3b0723ed5191d7cc5c6a765814c51c9d5090e
quadtree <quadtree@gmail.com>
2024-10-05 23:45:18 -0700

    U

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
 
-if [ "$TEST_FILE" ]; then
-    until touch "$TEST_FILE"
-    do
-        echo "Waiting for $TEST_FILE"
-        sleep 1
-    done
-fi
-
 set -xe
 
 source ~/emsdk/emsdk_env.sh
 
-LIBS_DIR="${LIBS_DIR:-/tmp/libs}"
 BUILD_DIR="${BUILD_DIR:-/tmp/build}"
 OUTPUT_DIR="${OUTPUT_DIR:-/tmp/output}"
 SOURCE_DIR="${SOURCE_DIR:-/src}"
 
 if [ ! -d "$BUILD_DIR" ]; then
     mkdir "$BUILD_DIR"
-fi
-
-if [ ! -d "$LIBS_DIR" ]; then
-    mkdir "$LIBS_DIR"
 fi
 
 if [ ! -d "$OUTPUT_DIR" ]; then
