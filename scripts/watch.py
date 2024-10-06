e966125a98e3d9b625a7f168cac6c90731a37d6e
quadtree <quadtree@gmail.com>
2024-10-05 19:59:12 -0700

    U

--- scripts/watch.py
+++ scripts/watch.py
@@ -1,18 +1,27 @@
+#!/usr/bin/python3
+
 import subprocess
 import threading
 import time
 
 inotify_proc = subprocess.Popen(["inotifywait", "-r", "-m", "-e", "move,modify,create,delete", "/src"], stdout=subprocess.PIPE)
 
-def listener_thread_entrypoint():
-    while True:
-        in_bytes = inotify_proc.stdout.read()
-        print(f'{in_bytes=}')
+last_change = 0
+last_compilation = -10000
 
-inotify_thread = threading.Thread(listener_thread_entrypoint)
+def listener_thread_entrypoint():
+    global last_change
+    print('listener_thread_entrypoint', flush=True)
+    while True:
+        inotify_proc.stdout.read(1)
+        last_change = time.time()
+
+inotify_thread = threading.Thread(target=listener_thread_entrypoint)
 inotify_thread.start()
 
 while True:
-    need_to_compile = False
+    if last_change > last_compilation - 0.1:
+        last_compilation = time.time()
+        subprocess.run(["python3", "/src/scripts/build.sh"])
 
-    time.sleep(1)
+    time.sleep(.1)
